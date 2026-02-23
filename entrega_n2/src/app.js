3e233fc0b7a3ca7191934577bdafecff63edb95d
fernandosalomon <fernandosalomon1992@gmail.com>
2026-02-23 15:21:18 -0300

    set handlebars view minimal config

--- entrega_n2/src/app.js
+++ entrega_n2/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+const handlebars = require("express-handlebars");
 PORT = 3030;
 
 const server = require("http").createServer(app);
@@ -7,6 +9,20 @@ const io = require("socket.io")(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "/views")));
+
+app.get("/", (req, res) => {
+  const test = {
+    name: "Producto de prueba",
+    description: "Producto de prueba",
+  };
+
+  res.render("index", test);
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
