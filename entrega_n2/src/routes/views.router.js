d5a1fdfc06fad5d77df9576538e5647fc64663fd
fernandosalomon <fernandosalomon1992@gmail.com>
2026-02-25 21:58:09 -0300

    add real-time products view and integrate product listing

--- entrega_n2/src/routes/views.router.js
+++ entrega_n2/src/routes/views.router.js
@@ -16,4 +16,13 @@ viewsRouter.get("/", async (req, res) => {
 
 })
 
+viewsRouter.get("/realtimeproducts", async (req, res) => {
+  try {
+    const products = await productManager.getAllProducts();
+    res.render("realTimeProducts", { products });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 module.exports = viewsRouter;
