29ac1905b1d476af0e2768fc8cb998ef715c1815
fernandosalomon <fernandosalomon1992@gmail.com>
2026-02-25 22:47:17 -0300

    feat: implement new product form submission and enhance UI elements

--- entrega_n2/src/public/js/client.js
+++ entrega_n2/src/public/js/client.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const newProductForm = document.getElementById("newProductForm");
+
+newProductForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(newProductForm);
+    const productData = {};
+
+    formData.forEach((value, key) => {
+        productData[key] = value;
+    });
+
+    console.log(productData);
+})
