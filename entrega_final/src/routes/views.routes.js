8e537740a6fee143cc4e40d1be85bc4e02796fcf
fernandosalomon <fernandosalomon1992@gmail.com>
2026-03-16 16:25:54 -0300

    feat: enhance homepage to display dynamic product listings and improve error handling

--- entrega_final/src/routes/views.routes.js
+++ entrega_final/src/routes/views.routes.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import Product from '../models/products.model.js';
 
 const viewsRouter = express.Router();
 
-viewsRouter.get("/", (req, res) => {
-    res.render("home")
+viewsRouter.get("/", async (req, res) => {
+
+    try {
+        const products = await Product.find().lean();
+        res.render("home", { products });
+    } catch (error) {
+        console.log(`Error al recuperar los productos (${error})`);
+    }
 })
 
 export default viewsRouter;
