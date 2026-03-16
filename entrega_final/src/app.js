3cf2e2084afb9795ea353828ca373a9d9ad1e357
Fernando F. Salomon <fernandosalomon1992@gmail.com>
2026-03-16 13:40:15 -0300

    feat: implement product model and database connection

--- entrega_final/src/app.js
+++ entrega_final/src/app.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const server = express();
-import handlebars from 'express-handlebars'
-import viewsRouter from './routes/views.routes.js';
-import dotenv from 'dotenv';
+import handlebars from "express-handlebars";
+import viewsRouter from "./routes/views.routes.js";
+import dotenv from "dotenv";
 dotenv.config();
-
 const PORT = process.env.PORT;
+
+import mongoConnect from "./configs/db.config.js";
+mongoConnect();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,5 +25,5 @@ server.set("views", path.join(__dirname, "views"));
 server.get("/", viewsRouter);
 
 server.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
-})
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
---
+++ entrega_final/src/models/products.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const ProductSchema = mongoose.Schema({
+  title: {
+    type: String,
+    minLength: 2,
+    maxLength: 25,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    minLength: 2,
+    maxLength: 100,
+    trim: true,
+    required: true,
+  },
+  code: {
+    type: String,
+    minLength: 6,
+    maxLength: 16,
+    uppercase: true,
+    trim: true,
+    required: true,
+  },
+  price: {
+    type: mongoose.Types.Decimal128,
+    min: 0,
+    required,
+  },
+  status: {
+    type: Boolean,
+    defaut: true,
+  },
+  stock: {
+    type: Number,
+    required,
+    min: 0,
+  },
+  category: {
+    type: String,
+    minLength: 2,
+    maxLength: 25,
+    trim: true,
+    required: true,
+  },
+  thumbnails: {
+    type: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    default: ["/public/img/default_product.png"],
+  },
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+
+export default Product;
