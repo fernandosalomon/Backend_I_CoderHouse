f8dbf3829bd3baa7f9e745660e4dd3fd52707530
fernandosalomon <fernandosalomon1992@gmail.com>
2026-03-16 22:27:45 -0300

    feat: add endpoint for delete products in cart and a whole cart

--- entrega_final/src/controllers/cart.controller.js
+++ entrega_final/src/controllers/cart.controller.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import Cart from "../models/cart.model.js";
 import Product from "../models/products.model.js";
 
-
 export const createNewCart = async (req, res) => {
   try {
     const createdCart = Cart.create({ products: [] });
@@ -33,32 +32,105 @@ export const getCartByID = async (req, res) => {
 };
 
 export const addProductToCart = async (req, res) => {
+  try {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const { quantity } = req.body;
+
+    const productData = await Product.findById(pid);
+    if (!productData)
+      res
+        .status(404)
+        .json({ status: "error", message: "Producto no encontrado" });
+
+    const cartData = await Cart.findById(cid);
+    if (!cartData)
+      res
+        .status(404)
+        .json({ status: "error", message: "Carrito no encontrado" });
+
+    const productIndex = cartData.products.findIndex(
+      (product) => product.product == pid,
+    );
+
+    if (productIndex == -1) {
+      cartData.products.push({
+        product: new mongoose.Types.ObjectId(pid),
+        quantity: quantity,
+      });
+    } else {
+      cartData.products[productIndex].quantity += quantity;
+    }
+
+    const addedProduct = await cartData.save({
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    res.status(201).json({ status: "success", payload: addedProduct });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: `Error al agregar el producto en el carrito (${error})`,
+      });
+  }
+};
+
+export const deleteProductFromCart = async (req, res) => {
+  try {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+
+    const cartData = await Cart.findById(cid);
+    if (!cartData)
+      res
+        .status(404)
+        .json({ status: "error", message: "Carrito no encontrado" });
+
+    const productIndex = cartData.products.findIndex(
+      (product) => product.product == pid,
+    );
+
+    if (productIndex == -1) res.status(404).json({status: "error", message: "Producto no encontrado en el carrito"})
+    
+    const deletedProduct = cartData.products[productIndex];
+    delete(cartData.products[productIndex]);
+
+    await cartData.save({
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    res.status(201).json({ status: "success", payload: deletedProduct });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: `Error al eliminar el producto en el carrito (${error})`,
+    });
+  }
+};
+
+export const deleteCart = async (req, res) => {
     try {
         const cid = req.params.cid;
-        const pid = req.params.pid;
-        const { quantity } = req.body;
+        
+        const deletedCart = await Cart.findByIdAndDelete(cid);
 
-        const productData = await Product.findById(pid);
-        if(!productData) res.status(404).json({status: "error", message: "Producto no encontrado"});
-
-        const cartData = await Cart.findById(cid);
-        if(!cartData) res.status(404).json({status: "error", message: "Carrito no encontrado"});
-
-        const productIndex = cartData.products.findIndex(product => product.product == pid);
-
-        if ( productIndex == -1){
-            cartData.products.push({product: new mongoose.Types.ObjectId(pid), quantity: quantity})
-        }else{
-            cartData.products[productIndex].quantity += quantity;
-        }
-
-        const addedProduct = await cartData.save({returnDocument: "after", runValidators: true});
-
-        res.status(201).json({status: "success", payload: addedProduct});
+        if (!deletedCart)
+          res
+            .status(404)
+            .json({ status: "error", message: "Carrito no encontrado" });
+        
+        res.status(200).json({status: "success", payload: deletedCart});
 
     } catch (error) {
-        res
-          .status(500)
-          .json({ status: "error", message: `Error al agregar el producto en el carrito (${error})` });
+        res.status(500).json({
+          status: "error",
+          message: `Error al eliminar el carrito (${error})`,
+        });
     }
 }
+
+
--- entrega_final/src/routes/cart.routes.js
+++ entrega_final/src/routes/cart.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { addProductToCart, createNewCart, getCartByID } from "../controllers/cart.controller.js";
+import { addProductToCart, createNewCart, deleteCart, deleteProductFromCart, getCartByID } from "../controllers/cart.controller.js";
 
 const cartRouter = express.Router();
 
 cartRouter.post("/", createNewCart);
 cartRouter.get("/:cid", getCartByID);
 cartRouter.post("/:cid/product/:pid", addProductToCart);
+cartRouter.delete("/:cid", deleteCart);
+cartRouter.delete("/:cid/product/:pid", deleteProductFromCart);
 
 
 export default cartRouter;
